03e1380aff6c038fc3b9bfc00c68ab89d90ed252
Yue Yang <g1enyy0ung@gmail.com>
2026-04-05 11:26:47 +0800

    v1.1.0

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   manifest: {
     name: '__MSG_extName__',
     description: '__MSG_extDescription__',
-    version: '1.0.3',
+    version: '1.1.0',
     default_locale: 'en',
     permissions: ['tabs', 'storage', 'contextMenus'],
     browser_specific_settings: {
