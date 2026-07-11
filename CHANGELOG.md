c30dc9619fe73b1a652704db951d8ee7549a9295
Yue Yang <g1enyy0ung@gmail.com>
2026-07-11 14:48:56 +0800

    v1.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.4.0] - 2026-07-11
+
 ### Added
 
 - Now users can edit the title of a tab.
--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export const commonPermissions = ['tabs', 'storage', 'contextMenus']
 export const manifest: UserManifest = {
   name: '__MSG_extName__',
   description: '__MSG_extDescription__',
-  version: '1.3.1',
+  version: '1.4.0',
   default_locale: 'en',
   commands: {
     open: {
